00983dab6391d137a58172cec1387c137c74129a
Bogomila <bkatsarska@abv.bg>
2022-11-02 11:16:30 +0200

    updated models.py, views.py, urls.py with different: - 'redirect-to-index', - IndexViewWithListView, - IndexViewWithTemplate, - Index2View.

--- cbv/web/urls.py
+++ cbv/web/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
-from cbv.web.views import IndexView, Index2View
+from django.views import generic as views
+from cbv.web.views import IndexView, Index2View, IndexViewWithTemplate, IndexViewWithListView
 
 urlpatterns = (
-    path('', IndexView.get_view()),
-    path('2/', Index2View.get_view()),
+    path('', IndexView.as_view()),
+    path('2/', Index2View.as_view()),
+    path('3/', IndexViewWithTemplate.as_view()),
+    path('4/', IndexViewWithListView.as_view()),
+    path('redirect-to-index/', views.RedirectView.as_view(url='/')),
 )
--- cbv/web/views.py
+++ cbv/web/views.py
@@ -1,29 +1,78 @@
 import random
-
+from django import views
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic as views
+
+from cbv.web.models import Employee
 
 
-def index():
-    pass
+def index(request):
+    context = {
+        'title': 'FBV',
+    }
+
+    return render(request, 'index.html', context)
 
 
-class IndexView:
-    # The __init__ method is the Python equivalent of the C++ constructor in an object-oriented approach.
-    # The __init__  function is called every time an object is created from a class.
-    # The __init__ method lets the class initialize the object’s attributes and serves no other purpose.
-    # It is only used within classes.
-    def __init__(self):
-        self.values = [
-            random.randint(1, 15),
-            ]
+class IndexView(views.View):
+    def get(self, request):
+        context = {
+            'title': 'Bare view',
+        }
 
-    @classmethod
-    def get_view(cls):
-        return cls().view
+        return render(request, 'index.html', context)
 
-    def view(self, request):
-        return HttpResponse(f'It works: {self.values}')
+    def post(self, request):
+        pass
+
+
+class IndexViewWithTemplate(views.TemplateView):
+    template_name = 'index.html'
+    extra_context = {
+        # static context
+        'title': 'Template view',
+    }
+    # Dynamic context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['employees'] = Employee.objects.all()
+        return context
+
+
+class IndexViewWithListView(views.ListView):
+    model = Employee
+    template_name = 'index.html'
+    context_object_name = 'employees'
+    extra_context = {
+        'title': 'List view',
+    }
+
+# def my_view(request):
+#     handler = None
+#     if request.method == 'GET':
+#         handler = self.get
+#     else:
+#         handler = self.post
+#     return handler
+
+# class IndexView:
+#     # The __init__ method is the Python equivalent of the C++ constructor in an object-oriented approach.
+#     # The __init__  function is called every time an object is created from a class.
+#     # The __init__ method lets the class initialize the object’s attributes and serves no other purpose.
+#     # It is only used within classes.
+#     def __init__(self):
+#         self.values = [
+#             random.randint(1, 15),
+#             ]
+#
+#     @classmethod
+#     def get_view(cls):
+#         return cls().view
+#
+#     def view(self, request):
+#         return HttpResponse(f'It works: {self.values}')
 
 
 class Index2View(IndexView):
